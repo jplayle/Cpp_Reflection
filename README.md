df7f861522eb49786934ac436a39907bf8f14256
jplayle <joshplayle@hotmail.com>
2023-12-30 14:48:55 +0000

    start readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Cpp_Reflection
-Reflection tools for C++ code
+C++ Reflection Tools
+
+    #include "reflect.hpp"
+    
+    struct  A
+    {
+    REFLECT(
+	    (int) x,
+	    (bool) y
+	)};
+
+Query members (constexpr):
+
+    reflection::member_view<A>::HAS(x);   // true
+    reflection::member_view<A>::has("x"); // true
+    reflection::member_view<A>::has("q"); // false
+
+Get size of all fields (constexpr):
+
+    sizeof_fields<A>::value; // sizeof(int) + sizeof(bool)
+
--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 #include "reflect.hpp"
 #include <iostream>
 
-using namespace reflection;
+using namespace ;
 
 struct A
 {
@@ -13,17 +13,17 @@ REFLECT(
 )};
 
 // query sum size of fields
-static_assert(sizeof_fields<A>::value == 5,     "");
+static_assert(sizeof_fields<A>::value == sizeof(int) + sizeof(bool), "");
 
 // query existence of members in struct by macro or stringified member name
-static_assert(member_view<A>::HAS(x)   == true,  "");
-static_assert(member_view<A>::has("x") == true,  "");
+static_assert(reflection::member_view<A>::HAS(x)   == true,  "");
+static_assert(reflection::member_view<A>::has("x") == true,  "");
 
-static_assert(member_view<A>::HAS(f)   == false, "");
-static_assert(member_view<A>::has("i") == false, "");
+static_assert(reflection::member_view<A>::HAS(f)   == false, "");
+static_assert(reflection::member_view<A>::has("i") == false, "");
 
 // observe type of member at position
-static_assert( std::is_same<int, reflector::field_t<0, A>::type>::value == true, "");
+static_assert( std::is_same<int, reflection::reflector::field_t<0, A>::type>::value == true, "");
 
 int main()
 {   
